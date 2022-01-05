c26e1e663b1fa76a68f08aa0e78c6de9ab406e32
Zikre Haimanot <zikrehaimanot@gmail.com>
2022-01-05 12:03:40 -0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates_presence_of :name, :email
+  validates :email, presence: true,
+                    email_format: { message: 'Invalid email format' }
+
+
+
 end
